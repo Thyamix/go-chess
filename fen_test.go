928540dcb140eeeba5fc5308bd9eb225af018e4e
Thyamix <contact@thyamix.com>
2026-05-12 12:01:29 +0200

    feat(WIP): fen tests starting positions

--- fen_test.go
+++ fen_test.go
@@ -14,15 +14,15 @@ func TestGetPosition(t *testing.T) {
 			fen: StartingPositionFEN,
 			check: func(t *testing.T, position *Position) {
 				if position.board[WHITE] != 0x000000000000FFFF {
-					t.Errorf("white pieces: got %d, want 65535", position.board[WHITE])
+					t.Errorf("white pieces: got %016X, want 0x000000000000FFFF", position.board[WHITE])
 				}
 			},
 		},
 		"starting position - black pieces": {
 			fen: StartingPositionFEN,
 			check: func(t *testing.T, position *Position) {
-				if position.board[BLACK] != 0xFF00000 {
-					t.Errorf("black pieces: got %d, want 18446462598732840960", position.board[BLACK])
+				if position.board[BLACK] != 0xFFFF000000000000 {
+					t.Errorf("black pieces: got %016X, want 0xFFFF000000000000", position.board[BLACK])
 				}
 			},
 		},
@@ -32,7 +32,7 @@ func TestGetPosition(t *testing.T) {
 			fen: StartingPositionFEN,
 			check: func(t *testing.T, position *Position) {
 				if position.pawns[WHITE] != 0x000000000000FF00 {
-					t.Errorf("white pawns: got %d, want 65280", position.pawns[WHITE])
+					t.Errorf("white pawns: got %016X, want 0x000000000000FF00", position.pawns[WHITE])
 				}
 			},
 		},
@@ -40,7 +40,7 @@ func TestGetPosition(t *testing.T) {
 			fen: StartingPositionFEN,
 			check: func(t *testing.T, position *Position) {
 				if position.pawns[BLACK] != 0x00FF000000000000 {
-					t.Errorf("black pawns: got %d, want 71776119061217280", position.pawns[BLACK])
+					t.Errorf("black pawns: got %016X, want 0x00FF000000000000", position.pawns[BLACK])
 				}
 			},
 		},
@@ -50,7 +50,7 @@ func TestGetPosition(t *testing.T) {
 			fen: StartingPositionFEN,
 			check: func(t *testing.T, position *Position) {
 				if position.knight[WHITE] != 0x0000000000000042 {
-					t.Errorf("white knight: got %d, want 66", position.knight[WHITE])
+					t.Errorf("white knight: got %016X, want 0x0000000000000042", position.knight[WHITE])
 				}
 			},
 		},
@@ -58,14 +58,92 @@ func TestGetPosition(t *testing.T) {
 			fen: StartingPositionFEN,
 			check: func(t *testing.T, position *Position) {
 				if position.knight[BLACK] != 0x4200000000000000 {
-					t.Errorf("black knight: got %d, want 4755801206503243776", position.knight[BLACK])
+					t.Errorf("black knight: got %016X, want 0x4200000000000000", position.knight[BLACK])
 				}
 			},
 		},
+
 		// Bishops
+		"starting position - white bishops": {
+			fen: StartingPositionFEN,
+			check: func(t *testing.T, position *Position) {
+				if position.bishops[WHITE] != 0x0000000000000024 {
+					t.Errorf("white bishops: got %016X, want 0x0000000000000024", position.bishops[WHITE])
+				}
+			},
+		},
+		"starting position - black bishops": {
+			fen: StartingPositionFEN,
+			check: func(t *testing.T, position *Position) {
+				if position.bishops[BLACK] != 0x2400000000000000 {
+					t.Errorf("black bishops: got %016X, want 0x2400000000000000", position.bishops[BLACK])
+				}
+			},
+		},
+
 		// Rooks
-		// Kings
+		"starting position - white rooks": {
+			fen: StartingPositionFEN,
+			check: func(t *testing.T, position *Position) {
+				if position.rooks[WHITE] != 0x0000000000000081 {
+					t.Errorf("white rooks: got %016X, want 0x0000000000000081", position.rooks[WHITE])
+				}
+			},
+		},
+		"starting position - black rooks": {
+			fen: StartingPositionFEN,
+			check: func(t *testing.T, position *Position) {
+				if position.rooks[BLACK] != 0x8100000000000000 {
+					t.Errorf("black rooks: got %016X, want 0x8100000000000000", position.rooks[BLACK])
+				}
+			},
+		},
+
 		// Queens
+		"starting position - white queens": {
+			fen: StartingPositionFEN,
+			check: func(t *testing.T, position *Position) {
+				if position.queens[WHITE] != 0x0000000000000010 {
+					t.Errorf("white queens: got %016X, want 0x0000000000000010", position.queens[WHITE])
+				}
+			},
+		},
+		"starting position - black queens": {
+			fen: StartingPositionFEN,
+			check: func(t *testing.T, position *Position) {
+				if position.queens[BLACK] != 0x1000000000000000 {
+					t.Errorf("black queens: got %016X, want 0x1000000000000000", position.queens[BLACK])
+				}
+			},
+		},
+
+		// Kings
+		"starting position - white kings": {
+			fen: StartingPositionFEN,
+			check: func(t *testing.T, position *Position) {
+				if position.kings[WHITE] != 0x0000000000000008 {
+					t.Errorf("white kings: got %016X, want 0x0000000000000008", position.kings[WHITE])
+				}
+			},
+		},
+		"starting position - black kings": {
+			fen: StartingPositionFEN,
+			check: func(t *testing.T, position *Position) {
+				if position.kings[BLACK] != 0x0800000000000000 {
+					t.Errorf("black kings: got %016X, want 0x0800000000000000", position.kings[BLACK])
+				}
+			},
+		},
+
+		// Custom position
+
+		// En passant
+
+		// Castling rights
+
+		// Move clock (half/full moves)
+
+		// Side to move
 	}
 
 	for test := range tests {
